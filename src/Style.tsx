5f4ab36264a6c00cea917a52f7f9cf0c08b4ee5f
mattisirokov <mattisirokov@gmail.com>
2023-06-07 10:32:10 +0300

    Styling change

--- src/Style.tsx
+++ src/Style.tsx
@@ -18,12 +18,12 @@ const Style = styled.div`
     &:after {
       content: '';
       display: block;
-      width: 30px;
-      height: 30px;
+      width: 10px;
+      height: 10px;
       border-radius: 30px;
       position: absolute;
       right: 3px;
-      background: linear-gradient(to top, #fff, #f2f2f2);
+      background: orange;
       border: 1px solid #ccc;
       top: -10px;
       transform: all ease 0.4s;
