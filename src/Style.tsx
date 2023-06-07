83b327086a5b4768a5f6da9e8489570a5894b0f8
mattisirokov <mattisirokov@gmail.com>
2023-06-07 13:57:57 +0300

    experimental cssr

--- src/Style.tsx
+++ src/Style.tsx
@@ -9,28 +9,21 @@ const Style = styled.div`
     opacity: 0.7;
   }
   .sliderHandle {
-    width: 50%;
     pointer-events: all;
     position: absolute;
     left: 50%;
     top: 50%;
     transform-origin: 0 50%;
-    &:after {
-      content: ' ';
-      display: block;
-      width: 20px;
-      height: 20px;
-      border-radius: 100%;
-      position: absolute;
-      right: -10px;
-      background: #cabd98;
-      border: 1px solid #ccc;
-      top: -10px;
-      transition: all ease 0.4s;
-    }
-    &:hover:after {
-      box-shadow: 0 0 10px rgb(37, 205, 247);
-    }
+    content: ' ';
+    display: block;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    right: -10px;
+    background: #cabd98;
+    border: 1px solid #ccc;
+    top: -10px;
+    transition: all ease 0.4s;
   }
   ${({ overrideStyle }) => overrideStyle}
 `;
