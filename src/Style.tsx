c02d09ef7a2e481d1c45f72f5661f7fee94c973d
mattisirokov <mattisirokov@gmail.com>
2023-06-07 16:26:49 +0300

    Original styles applied

--- src/Style.tsx
+++ src/Style.tsx
@@ -1,37 +1,26 @@
-// @ts-nocheck
-
 import styled from 'styled-components';
 
-const Style = styled.div`
-  display: inline-block;
-  position: relative;
-  svg path {
-    opacity: 0.7;
-  }
-  .sliderHandle {
-    width: 50%;
-    pointer-events: all;
+interface StyleProps {
+  overrideStyle?: string;
+}
+
+const Style = styled.div<StyleProps>`
+  // Your default styles...
+  .sliderHandle:after {
+    content: '';
+    display: block;
+    width: 30px;
+    height: 30px;
+    border-radius: 30px;
     position: absolute;
-    left: 50%;
-    top: 50%;
-    transform-origin: 0 50%;
-    &:after {
-      content: '';
-      display: block;
-      width: 30px;
-      height: 30px;
-      border-radius: 30px;
-      position: absolute;
-      right: 3px;
-      background: linear-gradient(to top, #fff, #f2f2f2);
-      border: 1px solid #ccc;
-      top: -10px;
-      transform: all ease 0.4s;
-    }
-    &:hover:after {
-      box-shadow: 0 0 10px rgb(37, 205, 247);
-    }
+    right: 3px;
+    background: linear-gradient(to top, #fff, #f2f2f2);
+    border: 1px solid #ccc;
+    top: -10px;
+    transform: all ease 0.4s;
   }
+
   ${({ overrideStyle }) => overrideStyle}
 `;
+
 export default Style;
